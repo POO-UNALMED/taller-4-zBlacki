a64afa88cef288ecd140d57204968545eb20eef9
José <69688188+zBlacki@users.noreply.github.com>
2020-09-04 21:44:06 -0500

    Add files via upload

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,16 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula = 1;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+    }
+    
+    Persona(){
+    	this.cedula = 0;
     }
 
     Persona(long cedula, String nombre) {
